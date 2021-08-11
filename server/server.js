162f2f2e85d5e8f83049cc0300ec962967f9e85c
CSesmeContif <csesme@contifico.com>
2021-08-10 23:15:51 -0500

    correccion

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   console.log(path.join(publicPath, '\\index.html'))
-   res.sendFile(path.join(publicPath, '\\index.html'));
+   console.log(path.join(publicPath, 'index.html'))
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
